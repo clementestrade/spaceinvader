68255699f4838d6251fc32bb15244b082984afcc
clement <clement@164.81.232.53>
2017-05-02 14:12:35 +0200

    aUnVaisseau

--- src/main/java/fr/unilim/iut/spacesinvader/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spacesinvader/SpaceInvaders.java
@@ -4,21 +4,38 @@ public class SpaceInvaders {
 
     int longueur;
     int hauteur;
-
+    Vaisseau vaisseau;
     public SpaceInvaders(int longueur, int hauteur) {
 	   this.longueur = longueur;
 	   this.hauteur = hauteur;
    }
+    
+    
+    
+    public void positionnerUnNouveauVaisseau(int x, int y) {
+        Vaisseau vaisseau = new Vaisseau(x,y);
+	}
 
     @Override
 	public String toString() {
 		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int i = 0; i < hauteur; i++) {
-			for (int j = 0; j < longueur; j++) {
-				espaceDeJeu.append('.');
+		
+		for (int y = 0; y < hauteur; y++) {
+			for (int x = 0; x < longueur; x++) {
+				
+				if (this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y))
+					espaceDeJeu.append('V');
+				else
+					espaceDeJeu.append('.');
 			}
 			espaceDeJeu.append('\n');
 		}
 		return espaceDeJeu.toString();
+	}
+
+
+
+	private boolean aUnVaisseau() {
+		return vaisseau!=null;
 	}
 }
--- src/test/java/fr/unilim/iut/spacesinvader/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spacesinvader/SpaceInvadersTest.java
@@ -20,5 +20,21 @@ public class SpaceInvadersTest {
 	    "...............\n" + 
 	    "...............\n" , spaceInvaders.toString());
         }
+   @Test
+	public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
+		SpaceInvaders spaceInvaders = new SpaceInvaders(15, 10);
+		spaceInvaders.positionnerUnNouveauVaisseau(7,9);
+		assertEquals("" + 
+		"...............\n" + 
+		"...............\n" +
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" , spaceInvaders.toString());
+	}
 
    }
