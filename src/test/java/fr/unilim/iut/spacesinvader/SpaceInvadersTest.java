fdffb9b61a99056b5adad8da2a39f22b6a9b2456
clement <clement@164.81.232.53>
2017-05-02 13:43:17 +0200

    1ereversion

---
+++ src/test/java/fr/unilim/iut/spacesinvader/SpaceInvadersTest.java
@@ -0,0 +1,24 @@
+package fr.unilim.iut.spacesinvader;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SpaceInvadersTest {
+
+   @Test
+   public void test_AuDebut_JeuSpaceInvaderEstVide() {
+	    SpaceInvaders spaceInvaders = new SpaceInvaders(15, 10);
+	    assertEquals("" + 
+	    "...............\n" + 
+	    "...............\n" +
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" , spaceInvaders.toString());
+        }
+
+   }
